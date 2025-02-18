446b74d8cfadea1d6138fcd2ef230c198e952163
katonnbo <katonbo02rr@gmail.com>
2025-02-18 22:58:06 +0900

    Add files via upload

---
+++ kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,13 @@
+package kadai_018;
+
+class KatoTaro_Chapter18 extends Kato_Chapter18 {
+    @Override
+    public void setGivenName() {
+        this.givenName = "太郎";
+    }
+
+    @Override
+    public void eachIntroduce() {
+        System.out.println("私はjavaが得意です");
+    }
+}
