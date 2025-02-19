9a41497c5ca22c175b5fb799eafc0ea135062002
katonnbo <katonbo02rr@gmail.com>
2025-02-19 20:47:52 +0900

    Add files via upload

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,13 @@
+package kadai_018;
+
+class KatoHanako_Chapter18 extends Kato_Chapter18 {
+    @Override
+    public void setGivenName() {
+        this.givenName = "花子";
+    }
+
+    @Override
+    public void eachIntroduce() {
+        System.out.println("趣味は読書です");
+    }
+}
---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,13 @@
+package kadai_018;
+
+class KatoIchiro_Chapter18 extends Kato_Chapter18 {
+    @Override
+    public void setGivenName() {
+        this.givenName = "一郎";
+    }
+
+    @Override
+    public void eachIntroduce() {
+        System.out.println("好きな食べ物はリンゴです");
+    }
+}
